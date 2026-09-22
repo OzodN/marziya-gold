44874c74e13a9e634da2d689904bfe9aca077b22
OzodN <ytnuritdinov@gmail.com>
2026-09-22 23:21:00 +0500

    feat(backend): initial Spring Boot scaffold

---
+++ backend/src/test/java/com/marziyagold/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marziyagold.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
